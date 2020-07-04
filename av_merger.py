08c3992014586c2288ed7c414814e2449f135b06
jeffanberg <jeff@penguin>
2020-07-04 02:54:13 -0700

    It works!

--- av_merger.py
+++ av_merger.py
@@ -1,6 +1,6 @@
 import sys
 import pathlib
-from moviepy.editor import *
+import moviepy.editor as mpy
 
 # grab the arguments
 first_arg = sys.argv[1]
@@ -25,28 +25,28 @@ if music_dir.exists() is False:
 #load up the array of photos to merge
 def photo_list():
     for photo_file in photo_dir.iterdir():
-        pFile = pathlib.Path(photo_file)
-        if pFile.suffixes == '.jpg' or '.png' or '.gif':
-            yield pFile
+        pFilePath = pathlib.Path(photo_file)
+        if pFilePath.suffixes == '.jpg' or '.png' or '.gif':
+            print(photo_file)
+            yield photo_file
 
-def avMerge(music_file, photo_file, num):
-    video = ImageClip(photo_file)
-    audio = AudioClip(music_file)
-    video.set_audio(audio)
-    video.write_videofile("songfile" + num + ".mp4")
+def avMerge(song, photo, num):
+    video = mpy.ImageClip(f"{photo}")
+    audio = mpy.AudioFileClip(f"{song}")
+    video = video.set_audio(audio)
+    video = video.set_duration(audio.duration)
+    print("Video duration is" + str(video.duration))
+    print(audio.duration)
+    video.write_videofile("songfile" + str(num) + ".mp4", fps=24)
 
 
 # loop through music folder
 num=0
 
 for music_file in music_dir.iterdir():
-    mFile = pathlib.Path(music_file)
-    if mFile.suffix == '.mp3':
-        try:
-            avMerge(mFile, photo_list(), num)
-            num += 1
-        except:
-            print('Something went wrong.')
-
-# use a yield to loop through photo folder?
+    print(music_file)
+    mFilePath = pathlib.Path(music_file)
+    if mFilePath.suffix == '.mp3':
+        avMerge(music_file, next(photo_list()), num)
+        num += 1
 
